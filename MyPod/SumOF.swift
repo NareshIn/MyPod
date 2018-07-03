4c670144f9f639b41a118fd015bc9da9b2e04f87
Amit Chatterjee <amit.chatterjee@gmail.com>
2018-07-03 12:09:40 +0530

    add func

---
+++ MyPod/SumOF.swift
@@ -0,0 +1,15 @@
+//
+//  SumOF.swift
+//  MyPod
+//
+//  Created by Amit Chatterjee on 3/7/18.
+//  Copyright © 2018 Amit Chatterjee. All rights reserved.
+//
+
+import Foundation
+
+public class SumOF : NSObject {
+    public func sum(a: Int, b: Int){
+        print("Sum: \(a + b)")
+    }
+}
